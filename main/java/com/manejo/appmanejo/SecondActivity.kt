93fbba6cc06782397b93c0133b6c0c65d71c4324
Santrosherun <139982100+Santrosherun@users.noreply.github.com>
2024-06-21 20:21:01 -0500

    main folder

---
+++ main/java/com/manejo/appmanejo/SecondActivity.kt
@@ -0,0 +1,25 @@
+package com.manejo.appmanejo
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val text = findViewById<TextView>(R.id.dato)
+        val dato: String? = intent.getStringExtra("dato")
+        text.text = dato
+
+    }
+}
