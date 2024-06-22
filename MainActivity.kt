d14f5fbd7aacb031ab41fba2fbd73bbef794eb68
Santrosherun <139982100+Santrosherun@users.noreply.github.com>
2024-06-22 11:23:20 -0500

    This file has the listView Implementation
    As well as OnItemClickListener etc

---
+++ MainActivity.kt
@@ -0,0 +1,38 @@
+package com.manejo.appmanejo
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    val array = arrayOf("1", "2", "3")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val listView:ListView = findViewById(R.id.list)
+        val adapter = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, array)
+        listView.adapter = adapter
+
+        listView.onItemClickListener = object : AdapterView.OnItemClickListener {
+            override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(applicationContext, position.toString(), Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+}
